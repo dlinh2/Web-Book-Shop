ceac9e3c354999db0d7f012f9d7375686dbaab23
dlinh2 <117155251+dlinh2@users.noreply.github.com>
2023-07-08 15:51:53 +0700

    Add files via upload

--- web_server/Header/Header.php
+++ web_server/Header/Header.php
@@ -39,6 +39,7 @@
             <li><a href="../Publisher/PublisherList.php">Nhà xuất bản</a></li>
             <li><a href="../Category/CategoryList.php">Nhóm sản phẩm</a></li>
             <li><a href="../Book/BookList.php">Sách</a></li>
+            <li><a href="../Order/ShowOrder.php">Quản lý đơn hàng</a></li>
         </ul>
     </div>
 </body>
