ac8373026c4600556295f335734992f76e5145c1
dT0210 <duthanh41025@gmail.com>
2023-07-09 00:27:24 +0700

    change rich editor config

--- web_server/Publisher/AddPublisher.php
+++ web_server/Publisher/AddPublisher.php
@@ -20,14 +20,8 @@
         });
         tinymce.init({
         selector: '#tPublisherDescription',
-        plugins: [
-          'a11ychecker','advlist','advcode','advtable','autolink','checklist','export',
-          'lists','link','image','charmap','preview','anchor','searchreplace','visualblocks',
-          'powerpaste','fullscreen','formatpainter','insertdatetime','media','table','help','wordcount'
-        ],
-        toolbar: 'undo redo | formatpainter casechange blocks | bold italic backcolor | ' +
-          'alignleft aligncenter alignright alignjustify | ' +
-          'bullist numlist checklist outdent indent | removeformat | a11ycheck code table help'
+        plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount',
+      toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat',
         });
     </script>
 </head>
